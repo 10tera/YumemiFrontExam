be99f09f22ab05e9bcf768f2ef608ad485e9df06
10tera <tyyt151123@gmail.com>
2023-03-15 16:36:38 +0900

    [Change]ビルド設定,初期設定

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/**
+ * @type import('webpack').Configuration
+ */
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname,"dist/"),
+        filename: "[name].js",
+    },
+    module: {
+        rules: [{
+            test: /\.ts[x]?$/ , use: "ts-loader"
+        }]
+    },
+    target: "web",
+    resolve: {
+        extensions: [".ts",".js",".tsx",".jsx"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            filename: "index.html",
+        }),
+    ],
+    devServer: {},
+    watch: true
+}
